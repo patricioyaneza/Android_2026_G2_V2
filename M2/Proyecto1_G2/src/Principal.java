5b25798053a5ae05c81169d278927a10afb15658
Patricio <patricio.yanez@fundacionluterana.cl>
2026-05-15 21:20:46 -0400

    update

---
+++ M2/Proyecto1_G2/src/Principal.java
@@ -0,0 +1,39 @@
+
+public class Principal {
+
+	// metodo
+	// este metodo es el punto de arranque de la aplicación
+	// entry point
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+		
+		// crear una variable
+		int numero = 10;
+		int numero2;
+		numero2 = 20;
+		System.out.print("\nValor de la variable numero: ");
+		System.out.println(numero);
+		System.out.println(numero2);
+		
+		numero = 30;
+		System.out.println(numero);
+		System.out.println("Numero 2 vale: " + numero2 );
+		
+		// numero = "HOla"; // error de compilación, no se puede asignar un string a una variable de tipo int
+		
+		String nombre = "Juan ";
+		String apellido;
+		apellido = "Perez";
+		
+		// Ejercicio 1: mostrar el nombre completo
+		System.out.print(nombre+apellido); // concatena strings (texto)
+		System.out.println("\nSu nombre es: " + nombre + apellido);
+		System.out.println("Su nombre es: " + nombre + " " + apellido);
+		
+		float numeroDecimal = 3.14159265f;
+		
+		System.out.println("El valor de pi es: " + numeroDecimal);
+	}
+	
+}
