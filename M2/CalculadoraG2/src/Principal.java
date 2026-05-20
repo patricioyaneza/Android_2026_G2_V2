0579a8c7dcd44eb31083df4cb048b83547cd4158
Patricio <patricio.yanez@fundacionluterana.cl>
2026-05-19 21:51:18 -0400

    update

---
+++ M2/CalculadoraG2/src/Principal.java
@@ -0,0 +1,18 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Ejercicio 4: Solicitar 2 numeros al usuario 
+		
+		int numero1 = 10;
+		int numero2 = 5;
+		
+		int resultadoSuma = numero1 + numero2;
+		System.out.println("| ***** Calculadora G2 ***** |");
+		System.out.println("La suma es: " + resultadoSuma);		
+		System.out.println("La resta es: " + (numero1 - numero2));
+	}
+
+}
