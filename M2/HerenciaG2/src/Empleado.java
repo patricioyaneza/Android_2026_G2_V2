765feaa65bebc2166e4d7d2081d5acb9a5a5bc15
Patricio <patricio.yanez@fundacionluterana.cl>
2026-06-10 21:27:38 -0400

    update

--- M2/HerenciaG2/src/Empleado.java
+++ M2/HerenciaG2/src/Empleado.java
@@ -1,6 +1,56 @@
 
 public class Empleado extends Persona {
+	// Ejercicio: Completar la clase Empleado
+	// crear 2 empleados en el main usando constructor con y sin parametros
+	
 	private int sueldo;
 	private double comision;
 	private String cargo;
+
+	public Empleado() {
+		super("", "", "");
+		this.sueldo = 0;
+		this.comision = 0;
+		this.cargo = "";
+	}
+		
+	public Empleado(String rut, String nombreCompleto, String direccion, int sueldo, double comision, String cargo) {
+		super(rut, nombreCompleto, direccion);
+		this.sueldo = sueldo;
+		this.comision = comision;
+		this.cargo = cargo;
+	}
+
+	public int getSueldo() {
+		return sueldo;
+	}
+
+	public void setSueldo(int sueldo) {
+		this.sueldo = sueldo;
+	}
+
+	public double getComision() {
+		return comision;
+	}
+
+	public void setComision(double comision) {
+		this.comision = comision;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	
+	public void imprimirDatos()
+	{
+		super.imprimirDatos();
+		System.out.println("Sueldo			: " + this.sueldo);
+		System.out.println("Comision		: " + this.comision);
+		System.out.println("Cargo			: " + this.cargo);
+	}
+	
 }
--- M2/HerenciaG2/src/Principal.java
+++ M2/HerenciaG2/src/Principal.java
@@ -11,7 +11,18 @@ public class Principal {
 		c1.setMetodoPago("Crédito");
 		c1.setMontoCredito(10000000);
 		
+		System.out.println("======== Información cliente 1 ========");
 		c1.imprimirDatos();
+		
+		
+		Cliente c2 = new Cliente("2000","Ana Pérez","Av Pajaritos 50", 5000000, "Cheque");
+		System.out.println("\n======== Información cliente 2 ========");
+		c2.imprimirDatos();
+		
+		
+		Empleado e1 = new Empleado("100", "Ana", "Providencia 50", 500000, 2, "Vendedora");	
+		System.out.println("\n======== Información Empleado 2 ========");
+		e1.imprimirDatos();
 	}
 
 }
