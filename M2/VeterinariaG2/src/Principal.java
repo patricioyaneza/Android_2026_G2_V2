ad8a1e645dbf04fa12c2a80e57d401c0ad976809
Patricio <patricio.yanez@fundacionluterana.cl>
2026-06-17 19:58:44 -0400

    update

--- M2/VeterinariaG2/src/Principal.java
+++ M2/VeterinariaG2/src/Principal.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Principal {
 
@@ -8,16 +9,48 @@ public class Principal {
 		
 		// super clase = Sub clase
 		Animal perro2 = new Perro("Cachupin", 20.5, "Galgo");
+		perro2.imprimir();
+		
 		
 		System.out.println("\n");
 		Gato gato1 = new Gato("Michi", 10.3, true);
 		gato1.imprimir();
 		
 		Animal gato2 = new Gato();
-		
-		
+				
 		perro1.emitirSonido();
 		gato1.emitirSonido();
+		
+		// Crea el ArrayList
+		ArrayList<Animal> veterinaria = new ArrayList<Animal>();
+		
+		// mascotas por agregar
+		Perro p1 = new Perro("Benji", 20, "golden");
+		Perro p2 = new Perro("ayudante de santa", 30, "San Bernardo");
+		Perro p3 = new Perro("Scrapy", 5, "pug");
+		
+		Gato g1 = new Gato("bola de nieve I", 10, true);
+		Gato g2 = new Gato("bola de nieve II", 12, true);
+		
+		// agregar mascotas a la coleccion (arraylist)
+		veterinaria.add(p1);
+		veterinaria.add(p2);
+		veterinaria.add(p3);
+		veterinaria.add(g1);
+		veterinaria.add(g2);
+		
+		// muestra la cantidad de elementos actual de la coleccion
+		System.out.println("cantidad de elementos: " + veterinaria.size() );
+		
+		// recorrer la colección
+		for(Animal aux: veterinaria)
+		{
+			System.out.println("Nombre de la mascota: " + aux.getNombre());
+
+		}
+		
+		
+		
 	}
 
 }
