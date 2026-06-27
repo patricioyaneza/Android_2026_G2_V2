6cd3325c48e26b33b0f0d088f23e68ec6f1ee87b
Patricio <patricio.yanez@fundacionluterana.cl>
2026-06-26 20:33:06 -0400

    update

--- M2/ProyectoPruebasUnitariasG2/src/CalculadoraTest.java
+++ M2/ProyectoPruebasUnitariasG2/src/CalculadoraTest.java
@@ -1,15 +1,31 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculadoraTest {
 
+	private static Calculadora calc;
+	@BeforeAll
+	static void configuracionGeneral()
+	{
+		calc = new Calculadora();
+	}
+	
+	@BeforeEach
+	void configuracion()
+	{
+		System.out.println("configuración ejecutada.-");
+	}
+	
 	@Test
 	void sumarSoloNumerosPositivos()
 	{
-		Calculadora c = new Calculadora();
-		int resultado = c.sumar(10, 20);
-		assertEquals(30, resultado);
+		System.out.println("Suma de calc: " + calc.sumar(10, 90));
+		Calculadora calculadora = new Calculadora();
+		int resultado = calculadora.sumar(2, 2);
+		assertEquals(4, resultado);
 	}
 
 	@Test
@@ -36,6 +52,23 @@ class CalculadoraTest {
 		int resultado = c.sumar(-5, 3);
 		assertEquals(-2, resultado);
 	}
+	
+
+	@Test
+	void restarSoloNumerosPositivos()
+	{
+		int resultado = calc.restar(20, 80);
+		System.out.println("resultado" + resultado);
+		assertEquals(-60, resultado);
+	}
+	// Ejercicios: crear 3 pruebas (escenarios) para la resta
+
+	@Test
+	void restarNumerosPositivosYNegativos() {	
+		int resultado = calc.restar(-10,20);
+		assertEquals(-30, resultado);
+	}
+	
 	
 	
 }
