a6f99d2c1681bd0abf6dbc36fb63d1e1ea95a703
Patricio <pa.yaneza@outlook.cl>
2026-07-24 20:25:43 -0400

    update

--- M4/Proyecto01/app/src/main/java/com/example/proyecto01/MainActivity2.java
+++ M4/Proyecto01/app/src/main/java/com/example/proyecto01/MainActivity2.java
@@ -1,24 +1,34 @@
 package com.example.proyecto01;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button btnSumar;
+    EditText txtNumero1;
+    EditText txtNumero2;
+    TextView lblResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnSumar = findViewById(R.id.btnSumar);
+
+        btnSumar.setOnClickListener(view -> {
+
+            Toast.makeText(this, "Presionó el botón sumar", Toast.LENGTH_LONG).show();
+
         });
     }
 }
