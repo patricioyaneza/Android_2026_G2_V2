1ae35eb45f2c827edf6c018d6a246ef3ceed118a
Patricio <pa.yaneza@outlook.cl>
2026-09-02 21:27:22 -0400

    update

---
+++ M5/ProyectoCorutinaG2/app/src/main/java/com/example/proyectocorutinag2/MainActivity2.kt
@@ -0,0 +1,40 @@
+package com.example.proyectocorutinag2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.proyectocorutinag2.databinding.ActivityMain2Binding
+
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain2Binding
+    var contador = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAumentar.setOnClickListener {
+
+            //Thread.sleep(3000)
+            binding.txtContador.text = (++contador).toString()
+        }
+        binding.btnSolicitar.setOnClickListener {
+            binding.txtMensaje.text = "Iniciado solicitud"
+
+            lifecycleScope.launch {
+                delay(10000)
+                binding.txtMensaje.text = "Solicitud Finalizada"
+            }
+        }
+
+    }
+}
